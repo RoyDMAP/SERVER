fcf1f6cf601431a6058f9df7e2ab4519b3d4f5d7
RoyDMAP <roy.dimapilis@sdgku.edu>
2024-07-31 19:47:47 -0700

    FSDI 107 class 3

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+import json
 
 app = Flask (__name__)
 
@@ -6,13 +7,43 @@ app = Flask (__name__)
 def home():
     return "hello from flask"
 
-
 @app.get("/about")
 def about():
     return "this is the about page rules"
 
-@app.get("/login")
-def login():
-    return "please provide your credentials"
+@app.get("/json")
+def name():
+    me = {"name": "Roy"}
+    return json.dumps(me)
+
+product = []
+@app.post("/api/products")
+def save_products():
+    items = request.get_json()
+    product.append(items)
+    print (items)
+    return json.dumps(items)
+
+@app.get("/api/products")
+def get_products():
+    return json.dumps(product)
+
+@app.put("/api/products/<int:index>")
+def update_products(index):
+    updated_products = request.get_json()
+    if 0<=index < len(product):
+        product[index] = updated_products
+        return json.dumps(updated_products)
+    else:
+        return "that index does not exist"
+
+@app.delete("/api/products/<int:index>")
+def delete_products(index):
+    delete_products = request.get_json
+    if 0<=index < len(product):
+        delete_products = product.pop(index)
+        return json.dumps(delete_products)
+    else:
+        return "that index does not exist"
 
 app.run(debug=True)
